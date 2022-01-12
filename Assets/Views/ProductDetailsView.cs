91ece4ece2d61b572d0f7482dbc92ad685eac6d3
chandan Kumar <eplayadda@gmail.com>
2022-01-13 01:31:08 +0530

    Product Details class Added

---
+++ Assets/Views/ProductDetailsView.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProductDetailsView : MonoBehaviour
+{
+    public Image image;
+    public Text productName;
+    public Text discription;
+    public Text rateTxt;
+    public Text priceTxt;
+    public Text orderPriceTxt;
+    public Button addToCartsBtn;
+}
